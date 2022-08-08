1b4f24b287ebf52a4ced39b3a3a10cf4c2093e2e
Pradeep8802 <102598014+Pradeep8802@users.noreply.github.com>
2022-08-08 07:32:33 +0530

    Update 3.3.c

--- Assignment1/codes/3.3.c
+++ Assignment1/codes/3.3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// a function which calculates y[i] takings array x and and i as parameters
+// A function which calculates y[i] takings array x and and i as parameters
 float calculatey(int* p1,int  n){
 	int y=0;
 	if(n==0){y =*p1;}
@@ -13,17 +13,18 @@ float calculatey(int* p1,int  n){
 
 int main(){
 	FILE* fp;
+    const int number_Of_Points=20;
     int x[] = {1,2,3,4,2,1};
-    float y[20];
+    float y[number_Of_Points];
     int i;
-    for(int i=0;i<20;i++){
+    for(int i=0;i<number_Of_Points;i++){
 		y[i]=calculatey(x,i);}
 
 	// opening file
     fp = fopen("y(n).dat","w");
 
 	// writing to the file
-    for(i=0;i<20;i++){
+    for(i=0;i<number_Of_Points;i++){
         fprintf(fp,"%f\n",y[i]);
     }
 	// closing file
